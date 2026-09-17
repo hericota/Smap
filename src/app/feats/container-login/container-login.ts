3384be5c98d557654411b7a2bab7b1860d41acd7
ANA BEATRIZ <oibibih@gmail.com>
2026-09-17 17:06:40 -0300

    formulario e style

--- src/app/feats/container-login/container-login.ts
+++ src/app/feats/container-login/container-login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormLogin } from './form-login/form-login';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [FormLogin],
+  imports: [FormLogin, RouterLink],
   selector: 'app-container-login',
   styleUrl: './container-login.css',
   templateUrl: './container-login.html',
