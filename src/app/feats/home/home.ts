7fe6416c1f54beda49c7fff07eab6d3c9eac5877
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-18 16:26:10 -0300

    Adiciona apps para home

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
+import { Header } from '../../component/header/header';
+import { Hero } from './hero/hero';
 
 @Component({
-  imports: [],
+  imports: [Footer, Header, Hero],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
