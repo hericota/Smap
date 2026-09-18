fe8b078435da57656fa113600878c801231a38c2
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-18 17:04:57 -0300

    Adiciona sessão de ocorrências

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../component/header/header';
 import { Hero } from './hero/hero';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [Footer, Header, Hero],
+  imports: [Footer, Header, Hero, RouterLink],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
