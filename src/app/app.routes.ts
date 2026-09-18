59f00a519449094115e66a0968211f88495d1f64
jordan <jordansnatos@gmail.com>
2026-09-18 16:21:16 -0300

    cria home, e estiliza o hero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Header } from './component/header/header';
 import { ContainerLogin } from './feats/container-login/container-login';
+import { Hero } from './feats/home/hero/hero';
 
 
 export const routes: Routes = [
   {path:'header', component:Header},
+  {path:'hero', component:Hero},
   {path: 'login' , component: ContainerLogin},
   { path: '', redirectTo: 'mapa', pathMatch: 'full' },
   {
