7d332e0296c4be96bf89fd372db3e0b46ad04ab8
ANA BEATRIZ <oibibih@gmail.com>
2026-09-22 14:22:55 -0300

    style(login): melhora a estilização visual das validações do formulário

--- src/app/feats/container-login/form-login/form-login.ts
+++ src/app/feats/container-login/form-login/form-login.ts
@@ -19,29 +19,9 @@ export class FormLogin {
   private router = inject(Router);
 
   loginForm = form(this.loginModel, (schemaPath) => {
-    required(schemaPath.email, { message: '*obrigatório!' });
-    email(schemaPath.email, { message: '*Insira um email válido!' });
-    required(schemaPath.password, { message: '*obrigatório!' });
-    maxLength(schemaPath.password, 16, { 
-  message: '*A senha deve ter no máximo 16 caracteres!' 
-});
-    // pattern(schemaPath.password, /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
-    //   message:
-    //     '*Mínimo 8 caracteres, 1 letra maiúscula, 1 número e 1 caractere especial!',
-    // });
-    minLength(schemaPath.password, 8, { message: '*Mínimo 8 caracteres!' })
-     pattern(schemaPath.password, /.*[A-Z].*/, {
-      message: '*Ao menos 1 letra maiúscula!',
-    });
-
-    pattern(schemaPath.password, /.*\d.*/, {
-      message: '*Ao menos 1 número!',
-    });
-
-    pattern(schemaPath.password, /.*[@$!%*?&].*/, {
-      message: '*Ao menos 1 caractere especial!',
-    });
-  
+    required(schemaPath.email, { message: '*' });
+    email(schemaPath.email, { message: '*' });
+    required(schemaPath.password, { message: '*' });
   });
   fazerLogin(event: SubmitEvent) {
     event.preventDefault();
