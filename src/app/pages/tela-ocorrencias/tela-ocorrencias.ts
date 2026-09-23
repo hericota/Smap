9e68d3bf0e11fb2e4cdc52315a45e1ed977d2334
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-23 14:03:29 -0300

    Cria componente para tela  de ocorrencias

---
+++ src/app/pages/tela-ocorrencias/tela-ocorrencias.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-tela-ocorrencias',
+  styleUrl: './tela-ocorrencias.css',
+  templateUrl: './tela-ocorrencias.html',
+})
+export class TelaOcorrencias {}
