51febf26da0e3772d0854a2c1b3eb3508728456f
ANA BEATRIZ <oibibih@gmail.com>
2026-09-23 14:39:44 -0300

    feats/ adiciona mapa na pagina de perfil de usuario

--- src/app/feats/profile user/profile-home/profile-home.ts
+++ src/app/feats/profile user/profile-home/profile-home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BottomNav } from '../../../components/bottom-nav/bottom-nav';
 import { RouterLink } from '@angular/router';
+import { MapaSeparado } from '../../../components/mapa-separado/mapa-separado';
 
 @Component({
-  imports: [BottomNav, RouterLink],
+  imports: [BottomNav, RouterLink, MapaSeparado],
   selector: 'app-profile-home',
   styleUrl: './profile-home.css',
   templateUrl: './profile-home.html',
