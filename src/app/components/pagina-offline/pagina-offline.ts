db561aa288b0cf32238a11bb54e0630155945c8a
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-23 16:53:13 -0300

    Cria serviço de network e componente de pagina offline

---
+++ src/app/components/pagina-offline/pagina-offline.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-pagina-offline',
+  styleUrl: './pagina-offline.css',
+  templateUrl: './pagina-offline.html',
+})
+export class PaginaOffline {}
