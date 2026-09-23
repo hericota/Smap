5afd1b68c86a6f546d1504554fdff9c9305e2d4b
ANA BEATRIZ <oibibih@gmail.com>
2026-09-23 01:08:09 -0300

    style(cadastro): adiciona estilos CSS para as validações

--- src/app/feats/container-cadastro/form-cadastro/form-cadastro.ts
+++ src/app/feats/container-cadastro/form-cadastro/form-cadastro.ts
@@ -1,11 +1,18 @@
 import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CadastroInterface } from './cadastro-interface';
-import { email, form, maxLength, minLength, pattern, required } from '@angular/forms/signals';
-
+import {
+  email,
+  form,
+  maxLength,
+  minLength,
+  pattern,
+  required,
+  FormField,
+} from '@angular/forms/signals';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, FormField],
   selector: 'app-form-cadastro',
   styleUrl: './form-cadastro.css',
   templateUrl: './form-cadastro.html',
@@ -25,15 +32,15 @@ export class FormCadastro {
   private router = inject(Router);
 
   cadastroForm = form(this.cadastroModel, (schemaPath) => {
-    required(schemaPath.nome);
-    required(schemaPath.sobreNome);
-    required(schemaPath.email);
-    required(schemaPath.cpf);
-    required(schemaPath.telefone);
-    required(schemaPath.cep);
+    required(schemaPath.nome, { message: '*' });
+    required(schemaPath.sobreNome, { message: '*' });
+    required(schemaPath.email, { message: '*' });
+    required(schemaPath.cpf, { message: '*' });
+    required(schemaPath.telefone, { message: '*' });
+    required(schemaPath.cep, { message: '*' });
     required(schemaPath.senha);
     required(schemaPath.check);
-    email(schemaPath.email);
+    email(schemaPath.email, { message: '*' });
     maxLength(schemaPath.senha, 16, {
       message: '*A senha deve ter no máximo 16 caracteres!',
     });
@@ -47,19 +54,19 @@ export class FormCadastro {
     pattern(schemaPath.senha, /.*[@$!%*?&].*/, {
       message: '*Ao menos 1 caractere especial!',
     });
-    pattern(schemaPath.cpf, /^\d{3}\.\d{3}\.\d{3}-\d{2}$/, { message: '*CPF inválido!' });
-    pattern(schemaPath.telefone, /^\(\d{2}\)\s\d{4,5}-\d{4}$/, { message: '*Telefone inválido!' });
-    pattern(schemaPath.cep, /^\d{5}-\d{3}$/, { message: '*CEP inválido!' });
-     
+    pattern(schemaPath.cpf, /^(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{11})$/, { message: '*CPF inválido!' });
+    pattern(schemaPath.telefone, /^(\(\d{2}\)\s?\d{4,5}-\d{4}|\d{10,11})$/, {
+      message: '*Telefone inválido!',
+    });
+    pattern(schemaPath.cep, /^(\d{5}-\d{3}|\d{8})$/, { message: '*CEP inválido!' });
   });
 
-  cadastrar(event: SubmitEvent){
-     event.preventDefault();
+  cadastrar(event: SubmitEvent) {
+    event.preventDefault();
     if (this.cadastroForm().invalid()) return;
     else {
       this.cadastroForm().reset();
       this.router.navigate(['/perfil-usuario']); //pagina principal de perfil do usuario
-      
     }
   }
 }
