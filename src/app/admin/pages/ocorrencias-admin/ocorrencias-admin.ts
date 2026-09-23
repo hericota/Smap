8889cde280971e9ce57bc2b55b8f0721dfe40ad5
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-09-23 17:11:08 -0300

    iclementando api

--- src/app/admin/pages/ocorrencias-admin/ocorrencias-admin.ts
+++ src/app/admin/pages/ocorrencias-admin/ocorrencias-admin.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ConsumoApi } from '../../../feats/posts/consumo-api';
+import { Ocorrencia } from '../../../feats/ocorrencia';
+import { environment } from '../../../../environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   imports: [],
@@ -6,4 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './ocorrencias-admin.css',
   templateUrl: './ocorrencias-admin.html',
 })
-export class OcorrenciasAdmin {}
+export class OcorrenciasAdmin {
+  protected consumoService = inject(ConsumoApi)
+  private readonly urlApi = `${environment.apiUrl.replace(/\/+$/, '')}/ocorrencias`;
+
+  private route = inject(ActivatedRoute);
+  
+  // metodo get
+   readonly ocorrencias = httpResource<Ocorrencia>[](
+        () => this.urlApi
+    )
+}
