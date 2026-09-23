d9ccb84bba529f1c4df179a1cadcc3470f47afb9
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-23 17:15:20 -0300

    Adiciona estrutura html e começa estilos da pagina offline

--- src/app/components/pagina-offline/pagina-offline.ts
+++ src/app/components/pagina-offline/pagina-offline.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-pagina-offline',
   styleUrl: './pagina-offline.css',
   templateUrl: './pagina-offline.html',
